0a1acaed65832f5da6d6b2fe57b13474d99418dd
Stephen Hall <stephenmhall@hotmail.com>
2025-01-04 12:08:38 +0000

    Update device_tracker.py with SourceType.GPS
    Home assistant depreciation change

--- custom_components/zero_motorcycles_integration/device_tracker.py
+++ custom_components/zero_motorcycles_integration/device_tracker.py
@@ -1,6 +1,6 @@
 """Support for tracking devices."""
 
-from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
+from homeassistant.components.device_tracker import SourceType.GPS
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
 
 from .const import DOMAIN, LOGGER
